7ed82973ff193b4e3659ce1db357e8d9ce7cd2e7
Clemens Neurauter <clemens.neurauter@outlook.com>
2021-11-19 22:30:33 +0100

    added map

--- app.py
+++ app.py
@@ -1,60 +1,46 @@
 import streamlit as st
-import datetime
+from datetime import *
 import requests
+import pandas as pd
+import numpy as np
 
-
-
+st.markdown(
 '''
-# TaxiFareModel front
+# Predict your Taxi for your next ride
 '''
+)
 
-st.markdown(''' ### Pickup time and date
-''')
+pickup_datetime = datetime.now()
+pickup_longitude = 0
+pickup_latitude = 0
+dropoff_longitude = 0
+dropoff_latitude = 0
+
+st.markdown(''' ### Pickup time and date''')
 #date and time
-d = st.date_input("Please provide a date & time", datetime.date(2019, 7, 6))
-st.write('Pickup date:', d)
+pickup_datetime = st.text_input('Please enter time & date', value=pickup_datetime)
 
-t = st.time_input('Set an alarm for', datetime.time(8, 45))
-st.write('Pickup time', t)
-
-st.markdown(''' ### Pickup location
-''')
+st.markdown(''' ### Pickup & dropoff location''')
 #pickup longitude
-pickup_longitude = st.number_input('Insert pickup longitude')
-st.write('The longitude for the pickup is ', pickup_longitude)
-
+pickup_longitude = st.text_input('Insert pickup longitude', value=pickup_longitude)
 #pickup latitude
-pickup_latitude = st.number_input('Insert pickup latitude')
-st.write('The latitude for the pickup is ', pickup_latitude)
-
-st.markdown(''' ### Dropoff location
-''')
+pickup_latitude = st.text_input('Insert pickup latitude', value=pickup_latitude)
 #dropoff longitude
-dropoff_longitude = st.number_input('Insert dropoff longitude')
-st.write('The longitude for the dropoff is ', dropoff_longitude)
-
+dropoff_longitude = st.text_input('Insert dropoff longitude', value=dropoff_longitude)
 #dropoff latitude
-dropoff_latitude = st.number_input('Insert dropoff latitude')
-st.write('The latitude for the dropoff is ', dropoff_latitude)
+dropoff_latitude = st.text_input('Insert dropoff latitude', value=dropoff_latitude)
 
-st.markdown(''' ### Number of passengers
-''')
+st.markdown(''' ### Number of passengers''')
 #passenger count
-passenger_count = st.number_input('Insert number of passenger')
-st.write(passenger_count, ' passengers')
+passenger_count = int(st.selectbox('Select number of passenger', [1,2,3,4,5,6,7,8,9]))
 
 
-col1, col2, col3 = st.columns(3)
-col1.metric("Pickup Location", pickup_longitude)
-col2.metric("FTEC", "$121.10", "0.46%")
-col3.metric("BTC", "$46,583.91", "+4.87%")
 
 st.markdown('''
-            ## Predicted Fare Price
-''')
+            ## Predicted Fare Price ''')
 
 params = dict(key = 'anything',
-                pickup_datetime= f"{d} {t}",
+                pickup_datetime=pickup_datetime,
                 pickup_longitude=pickup_longitude,
                 pickup_latitude=pickup_latitude,
                 dropoff_longitude=dropoff_longitude,
@@ -71,5 +57,22 @@ if response.status_code == 200:
 else:
     print("API call error")
 
-response.status_code, response.json().get("prediction",
-                                            "no prediction"), response.json()
+response.status_code
+predicted_price = response.json().get("prediction",
+                                            "no prediction")
+st.markdown(f'# {predicted_price}')
+
+@st.cache
+def get_map_data():
+
+
+    return pd.DataFrame(
+        dict(lat=[float(pickup_latitude),
+                  float(dropoff_latitude)],
+             lon=[float(pickup_longitude),
+                  float(dropoff_longitude)]))
+
+
+df = get_map_data()
+
+st.map(df)
