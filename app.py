f0a6259fff0c7f480762196006ee05d5631e2382
Clemens Neurauter <clemens.neurauter@outlook.com>
2021-11-19 20:11:05 +0100

    code ready to be deployed to production

--- app.py
+++ app.py
@@ -1,55 +1,11 @@
 import streamlit as st
 import datetime
 import requests
-import joblib
 
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+
 
 '''
 # TaxiFareModel front
-'''
-
-st.markdown('''
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
-
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
-
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
-
-url = 'https://taxifare.lewagon.ai/predict'
-
-if url == 'https://taxifare.lewagon.ai/predict':
-
-    st.markdown(
-        'Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...'
-    )
-'''
-
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
 '''
 
 st.markdown(''' ### Pickup time and date
@@ -88,20 +44,24 @@ passenger_count = st.number_input('Insert number of passenger')
 st.write(passenger_count, ' passengers')
 
 
-
+col1, col2, col3 = st.columns(3)
+col1.metric("Pickup Location", pickup_longitude)
+col2.metric("FTEC", "$121.10", "0.46%")
+col3.metric("BTC", "$46,583.91", "+4.87%")
 
 st.markdown('''
             ## Predicted Fare Price
 ''')
+
 params = dict(key = 'anything',
-              pickup_datetime= f"{d} {t}",
-              pickup_longitude=pickup_longitude,
-              pickup_latitude=pickup_latitude,
-              dropoff_longitude=dropoff_longitude,
-              dropoff_latitude=dropoff_latitude,
-              passenger_count=int(passenger_count))
+                pickup_datetime= f"{d} {t}",
+                pickup_longitude=pickup_longitude,
+                pickup_latitude=pickup_latitude,
+                dropoff_longitude=dropoff_longitude,
+                dropoff_latitude=dropoff_latitude,
+                passenger_count=int(passenger_count))
 
-
+url = 'https://taxifare.lewagon.ai/predict'
 # retrieve the response
 response = requests.get(url, params=params)
 
@@ -112,4 +72,4 @@ else:
     print("API call error")
 
 response.status_code, response.json().get("prediction",
-                                          "no prediction"), response.json()
+                                            "no prediction"), response.json()
